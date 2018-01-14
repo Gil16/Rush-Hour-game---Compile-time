6e3e408b063097e334261fdc3253632694c817b8
Gil16 <gilbli166@gmail.com>
2018-01-14 20:16:41 +0200

    Part A is done!

--- Subject.h
+++ Subject.h
@@ -1,16 +1,19 @@
 #ifndef OOP5_SUBJECT_H
 #define OOP5_SUBJECT_H
 
+#include "Observer.h"
 #include "OOP5EventException.h"
 #include "list"
 #include <algorithm>
 
 template <typename T>
 class Subject {
-    std::list<Observer<T>> *observers;
+    std::list<Observer<T>*> *observers;
 
 public:
-    Subject() : observers() {}
+    Subject() {
+        this->observers = new std::list<Observer<T>*>;
+    }
     ~Subject(){
         delete observers;
     }
@@ -25,23 +28,23 @@ public:
 
 template<typename T>
 void Subject<T>::notify(const T& message) {
-    for(auto it = observers->begin() ; it != observers->end() ; ++it){
+    for(Observer<T>* it : *observers){
         it->handleEvent(message);
     }
 }
 
 template<typename T>
 void Subject<T>::addObserver(Observer<T>& ob) {
-    if((std::find(observers->begin(), observers->end(), ob) != observers->end())){
+    if((std::find(observers->begin(), observers->end(), &ob) != observers->end())){
         throw ObserverAlreadyKnownToSubject();
     }
-    observers->push_back(ob);
+    observers->push_back(&ob);
 }
 
 template<typename T>
 void Subject<T>::removeObserver(Observer<T>& ob) {
-    if((std::find(observers->begin(), observers->end(), ob)) != observers->end()){      // theoretical ?end?
-        observers->remove(ob);
+    if((std::find(observers->begin(), observers->end(), &ob)) != observers->end()){      // theoretical ?end?
+        observers->remove(&ob);
     }
     throw ObserverUnknownToSubject();
 }
