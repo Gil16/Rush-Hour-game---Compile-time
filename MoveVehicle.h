ee754c2665fbba6a8924dd287dd7fea1d25f20aa
Gil16 <gilbli166@gmail.com>
2018-01-15 14:09:12 +0200

    still coding part B

--- MoveVehicle.h
+++ MoveVehicle.h
@@ -1,12 +1,20 @@
 #ifndef OOP5_MOVEVEHICLE_H
 #define OOP5_MOVEVEHICLE_H
 
-template<typename T, typename D, int N>
+#include "CellType.h"
+
+template<CellType T, typename D, int N> // (**) not sure if CellType should be written here, but it works!
 struct Move<T, D, N>{
-    static_assert();
+    static_assert(T != EMPTY, "The Car type you enter was EMPTY, and it's not allowed!");
+    typedef CellType type = T;  // same as the first note (**)
     typedef D direction;
     constexpr static int amount = N;
 };
 
+
+template<>
+struct MoveVehicle<>{
+    
+};
 
 #endif //OOP5_MOVEVEHICLE_H
